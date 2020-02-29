07f1858c12c03135840ab1b9b39be3136944fc83
tallon <352420160@qq.com>
2020-03-01 00:37:08 +0800

    初始化服务提供者

--- provider/ums-admin-provider-service/src/main/java/com/funtl/myshop/plus/provider/service/UmsAdminServiceImpl.java
+++ provider/ums-admin-provider-service/src/main/java/com/funtl/myshop/plus/provider/service/UmsAdminServiceImpl.java
@@ -2,7 +2,9 @@ package com.funtl.myshop.plus.provider.service;
 
 import com.funtl.myshop.plus.provider.api.UmsAdminService;
 import com.funtl.myshop.plus.provider.domain.UmsAdmin;
+import org.apache.dubbo.config.annotation.Service;
 
+@Service(version = "1.0.0")
 public class UmsAdminServiceImpl implements UmsAdminService {
     @Override
     public int insert(UmsAdmin admin) {
